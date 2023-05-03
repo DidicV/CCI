993c86b8ce13d74d6032f405b12eee079f7e58b4
Didic Victor <75629328+DidicV@users.noreply.github.com>
2023-05-03 09:27:17 +0300

    Added controllers

---
+++ CCI/Controller/DisciplineController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CCI.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DisciplineController : ControllerBase
+    {
+        public DisciplineController()
+        {
+
+        }
+    }
+}
---
+++ CCI/Controller/StudentController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CCI.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        public StudentController()
+        {
+
+        }
+
+
+    }
+}
