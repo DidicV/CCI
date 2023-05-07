b942484213443cdb871694aa2c7392b765d06976
Didic Victor <75629328+DidicV@users.noreply.github.com>
2023-05-07 19:44:02 +0300

    Plotly JS call

---
+++ CCI/wwwroot/js/Chart.js
@@ -0,0 +1,24 @@
+﻿function GeneratePieChart(items) {
+
+
+
+
+    data = [{
+        type: 'scatterpolar',
+        r: [39, 28, 8, 7, 28, 39],
+        theta: ['A', 'B', 'C', 'D', 'E', 'A'],
+        fill: 'toself'
+    }]
+
+    layout = {
+        polar: {
+            radialaxis: {
+                visible: true,
+                range: [0, 50]
+            }
+        },
+        showlegend: false
+    }
+
+    Plotly.newPlot("chartdiv", data, layout)
+}
