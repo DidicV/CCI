521f23f4b50c955f0afeae1f8bffe03ee75809cb
Didic Victor <75629328+DidicV@users.noreply.github.com>
2023-05-13 16:55:58 +0300

    Cartesian system two disciplines

---
+++ CCI/wwwroot/js/Cartesian.js
@@ -0,0 +1,28 @@
+﻿function GenerateCartesianChart(mediaX, mediaY, students, titleX, titleY) {
+
+    var trace1 = {
+        x: mediaX,
+        y: mediaY,
+        mode: 'markers',
+        type: 'scatter',
+        text: students
+    };
+
+    var data = [trace1];
+
+    var layout = {
+        xaxis: {
+            rangemode: 'tozero',
+            range: [0, 10],
+            title: titleX
+        },
+        yaxis: {
+            rangemode: 'tozero',
+            range: [0, 10],
+            title: titleY
+        },
+        title: 'Union of two disciplines'
+    };
+
+    Plotly.newPlot('cartesian', data, layout);
+}
