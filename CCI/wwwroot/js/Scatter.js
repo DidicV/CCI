06af9b413023d46a7ce8dd4b5da43c771ab8e8bf
Didic Victor <75629328+DidicV@users.noreply.github.com>
2023-05-15 00:12:37 +0300

    Grade evolution

---
+++ CCI/wwwroot/js/Scatter.js
@@ -0,0 +1,17 @@
+﻿function GenerateScatterChart(data) {
+
+
+    data.forEach((item) => {
+        item.line = { shape: 'spline' }
+    });
+
+
+    var layout = {
+        title: 'Grades evolution',
+        yaxis: {
+            range: [0, 10.5]
+        },
+    };
+
+    Plotly.newPlot('scatter', data, layout);
+}
