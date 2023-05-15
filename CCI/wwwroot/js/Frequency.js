416b1a66d61805041949b1c7d414e495cbf94cca
Didic Victor <75629328+DidicV@users.noreply.github.com>
2023-05-16 00:11:29 +0300

    Update grade frequency of discipline

--- CCI/wwwroot/js/Frequency.js
+++ CCI/wwwroot/js/Frequency.js
@@ -1,19 +1,30 @@
 ﻿function GenerateFrequencyChart(grades) {
 
+
+
+    // Create an array to store the frequency of each grade
+    var frequency = new Array(11).fill(0);
+
+    // Count the frequency of each grade
+    grades.forEach(function (grade) {
+        frequency[grade]++;
+    });
+
     var trace = {
-        x: grades,
-        type: 'histogram',
-        histfunc: 'count'
+        x: Array.from({ length: 10 }, (_, i) => i + 1),
+        y: frequency.slice(1),
+        type: 'bar'
     };
 
     var layout = {
         title: 'Histogram with Frequency',
-        xaxis: { title: 'Value', tickformat: 'd' },
-        yaxis: { title: 'Frequency' },
-        bargap: 0.1 
+        xaxis: { title: 'Grade' },
+        yaxis: { title: 'Frequency' }
     };
 
     var data = [trace];
 
     Plotly.newPlot('frequency', data, layout);
+
+
 }
