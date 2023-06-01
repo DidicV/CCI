95814ab5919ab31f38c7622a364d29403db3e80a
Didic Victor <75629328+DidicV@users.noreply.github.com>
2023-06-01 11:55:45 +0300

    Vector distance

---
+++ CCI/Model/UploadedFile.cs
@@ -0,0 +1,9 @@
+﻿namespace CCI.Model
+{
+    public class UploadedFile
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+        public Dictionary<string, int> WordFrequencies { get; set; } = new();
+    }
+}
