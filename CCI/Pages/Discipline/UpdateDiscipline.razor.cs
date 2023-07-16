1e8174e35d1be9eea539cd4fa0f1ff04dc70b899
Victor Didic <75629328+DidicV@users.noreply.github.com>
2023-07-16 16:59:15 +0300

    Discipline update separation

---
+++ CCI/Pages/Discipline/UpdateDiscipline.razor.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace CCI.Pages.Discipline
+{
+    public partial class UpdateDiscipline
+    {
+        [Parameter]
+        public Guid Id { get; set; }
+
+        public Model.Discipline? Discipline { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            try
+            {
+                Discipline = await DisciplineService.GetDisciplineById(Id);
+            }
+            catch
+            {
+
+            }
+        }
+
+        async Task HandleSubmit()
+        {
+            if (Discipline != null)
+            {
+                await DisciplineService.UpdateDiscipline(Discipline);
+            }
+        }
+    }
+}
